e7ad6858dc4c55aa86e5b485aac9e06d5121fb58
iqramahamed <iqra.mahamed64@gmail.com>
2024-04-16 19:06:17 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name, email) {
+    const url = "http://localhost:3000/users";
+    const data = {name: name, email: email}
+
+    return fetch(url, {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }, 
+        body:JSON.stringify(data)
+
+    })
+    .then (response => response.json())
+    .then(data => {
+        const id = data.id;
+        console.log('New ID:', id);
+        document.body.innerHTML += `<p>New ID: ${id} <p>`
+    })
+    .catch(error => {
+        console.log('Error', error);
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`
+    });
+}
